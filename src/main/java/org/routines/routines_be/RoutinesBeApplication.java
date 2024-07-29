f7a64cfcf22f8687e77095d29e3406281be87e5a
Hoyeon <86115801+Hoyeon9@users.noreply.github.com>
2024-07-29 17:46:01 +0900

    Initialize Project

---
+++ src/main/java/org/routines/routines_be/RoutinesBeApplication.java
@@ -0,0 +1,13 @@
+package org.routines.routines_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoutinesBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoutinesBeApplication.class, args);
+    }
+
+}

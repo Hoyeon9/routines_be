011d5efd8af0ca827118afd2da507569e8d5ce8a
Hoyeon <86115801+Hoyeon9@users.noreply.github.com>
2024-07-31 23:28:01 +0900

    Fix body getting format

--- src/main/java/org/routines/routines_be/controller/RoutineController.java
+++ src/main/java/org/routines/routines_be/controller/RoutineController.java
@@ -1,5 +1,6 @@
 package org.routines.routines_be.controller;
 
+import org.routines.routines_be.dto.UserIdDTO;
 import org.routines.routines_be.entity.Routine;
 import org.routines.routines_be.service.RoutineService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,8 +17,14 @@ public class RoutineController {
     @Autowired
     private RoutineService routineService;
 
+    @GetMapping
+    public String getAny() {
+        return "Hello World";
+    }
+
     @GetMapping("/all")
-    public List<Routine> getAll(@RequestBody String userId) {
-        return routineService.getAllRoutines(userId);
+    public List<Routine> getAll(@RequestBody UserIdDTO userIdDTO) {
+        System.out.println(userIdDTO.getUserId());
+        return routineService.getAllRoutines(userIdDTO.getUserId());
     }
 }
---
+++ src/main/java/org/routines/routines_be/dto/UserIdDTO.java
@@ -0,0 +1,8 @@
+package org.routines.routines_be.dto;
+
+import lombok.Data;
+
+@Data
+public class UserIdDTO {
+    private String userId;
+}

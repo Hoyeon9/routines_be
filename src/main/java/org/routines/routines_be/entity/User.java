2e19988b50880a922e93e7f99c656fd669b920e4
Hoyeon <86115801+Hoyeon9@users.noreply.github.com>
2024-07-31 21:01:24 +0900

    Add @Table

--- src/main/java/org/routines/routines_be/entity/User.java
+++ src/main/java/org/routines/routines_be/entity/User.java
@@ -1,15 +1,13 @@
 package org.routines.routines_be.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @Entity
+@Table(name = "users")
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
